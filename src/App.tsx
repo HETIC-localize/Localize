2a7b7478422e6f6be125122826c039b28a403a17
Charlebois Christophe <dianketohead@gmail.com>
2022-03-11 10:09:27 +0100

    test something

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import logo from './logo.svg';
 
 import { useState } from "react";
 import { TranslationContext } from './context/TranslationContext'
@@ -6,13 +7,29 @@ import Translate from "./components/Translate";
 import languageData from "./components/languagedata.json"
 import {TraductionDataFormat} from './components/type'
 
-const test = 'home.statut'
+const world = 'home.world'
 
 function App() {
   const [translationData, setTranslationData] = useState<Array<TraductionDataFormat>>(languageData)
   return (
     <TranslationContext.Provider value= {{ translationData, setTranslationData}}>
-      <Translate transmittedCode={test} />
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+         <Translate transmittedCode={world} />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
     </TranslationContext.Provider>
   );
 }
